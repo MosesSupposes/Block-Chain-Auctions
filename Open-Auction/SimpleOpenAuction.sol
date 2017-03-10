dd871fcc00ae8b8fb82224eb93016aeec1415b71
Moses Samuel <mosamuel98@gmail.com>
2017-03-09 18:53:45 -0800

    refactor

--- Open-Auction/SimpleOpenAuction.sol
+++ Open-Auction/SimpleOpenAuction.sol
@@ -1,6 +1,6 @@
-/* The general lidea od the following simple auction contract is that everyone can send their bids during a bidding period. The bids already include sending money / ether in order to bind the bidders to their bid.If the highest bid is raised, the previously highest bidder gets her money back. After the end of the bidding period, the contract has to be called manually for the beneficiary to receive his money - contracts cannot activate themselves. */
-
 pragma solidity ^0.4.0;
+
+/* The general idea od the following simple auction contract is that everyone can send their bids during a bidding period. The bids already include sending money / ether in order to bind the bidders to their bid.If the highest bid is raised, the previously highest bidder gets her money back. After the end of the bidding period, the contract has to be called manually for the beneficiary to receive his money - contracts cannot activate themselves. */
 
 contract SimpleAuction {
     /* Paramaters of the auction. 
